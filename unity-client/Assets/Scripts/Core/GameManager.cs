98aea26a6c9f4d3e4bcc86117b2a270d19e2e780
kn134133 <46958161+kn134133@users.noreply.github.com>
2026-03-28 09:13:05 +0800

    Rewrite GameManager: simple singleton

---
+++ unity-client/Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this) { Destroy(gameObject); return; }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        Debug.Log("[GameManager] 滇行记初始化完成");
+    }
+}
